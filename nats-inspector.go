cdfc19014fc7c57d55b7b35c541c15c7f8a0ce5b
Ben Chacko <bchacko@pivotal.io>
2020-01-14 13:29:56 -0500

    allow user to enter the nats server url

--- nats-inspector.go
+++ nats-inspector.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "sync"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"sync"
 
-  nats "github.com/nats-io/nats.go"
+	nats "github.com/nats-io/nats.go"
 )
 
 type NatCommand struct {
@@ -16,51 +17,65 @@ type NatCommand struct {
 }
 
 func main() {
+
+	server := flag.String("server", "", "NATs server to target")
+	agent := flag.String("agent", "", "Agent ID to filter on")
+	flag.Parse()
+
 	msgJSON := NatCommand{}
 
-// Connect to NATS using client credentials
-fmt.Println("Connecting to NATS using client credentials...")
-var nc1, err = nats.Connect("nats://10.193.69.11:4222",
-			nats.ClientCert("./nats_cert.pem", "./nats_private.key"),
-			nats.RootCAs("./nats_ca_cert.pem"))
+	// Connect to NATS using client credentials
+	fmt.Println("Connecting to NATS using client credentials...")
+	var nc1, err = nats.Connect(*server+":4222",
+		nats.ClientCert("./nats_cert.pem", "./nats_private.key"),
+		nats.RootCAs("./nats_ca_cert.pem"))
 
-if err != nil {
-    fmt.Println("Oh boy!")
-} else {
-    fmt.Println("We gooood....")
-}
-
-// Connect to Director NATS
-fmt.Println("Connecting to NATS using director credentials...")
-var nc2, err2 = nats.Connect("nats://10.193.69.11:4222",
-                        nats.ClientCert("./nats_director_cert.pem", "./nats_director_private.key"),
-                        nats.RootCAs("./nats_director_ca_cert.pem"))
-
-if err2 != nil {
-    fmt.Println("Oh boy!")
-} else {
-    fmt.Println("We gooood....")
-}
-
-
-var startwg sync.WaitGroup
-startwg.Add(1)
-// Subscribe to agent directives from director
-fmt.Println("Subscribing to agent directives from director")
-nc1.Subscribe("agent.24fd2d5b-afc4-43b6-8554-d95d344e81e7", func(m *nats.Msg) {
-	fmt.Printf("Received message from Director: %s\n\n", string(m.Data))
-
-	err := json.Unmarshal([]byte(m.Data), &msgJSON)
 	if err != nil {
-		panic(err)
+		fmt.Println("Oh boy!")
+	} else {
+		fmt.Println("We gooood....")
 	}
-});
 
-// Subscribe to agent messages to director
-fmt.Println("Subscribing to agent messages to director")
-nc2.Subscribe("director.>", func(m *nats.Msg) {
-	fmt.Printf("Received message from agent: %s\n\n\n", string(m.Data))
-});
-startwg.Wait()
+	// Connect to Director NATS
+	fmt.Println("Connecting to NATS using director credentials...")
+	var nc2, err2 = nats.Connect(*server+":4222",
+		nats.ClientCert("./nats_director_cert.pem", "./nats_director_private.key"),
+		nats.RootCAs("./nats_director_ca_cert.pem"))
 
+	if err2 != nil {
+		fmt.Println("Oh boy!")
+	} else {
+		fmt.Println("We gooood....")
+	}
+
+	var startwg sync.WaitGroup
+	startwg.Add(1)
+
+	fmt.Println(*agent)
+	if *agent != "" {
+		// Subscribe to agent directives from director
+		fmt.Println("Subscribing to agent directives from director")
+		nc1.Subscribe(*agent, func(m *nats.Msg) {
+			fmt.Printf("Received message from Director: %s\n\n", string(m.Data))
+
+			err := json.Unmarshal([]byte(m.Data), &msgJSON)
+			if err != nil {
+				panic(err)
+			}
+		})
+	} else {
+		fmt.Println("Will not subscribe to director messages as agent id was not supplied...")
+	}
+
+	// Subscribe to agent messages to director
+	fmt.Println("Subscribing to agent messages to director")
+	nc2.Subscribe("director.>", func (m *nats.Msg) {
+		fmt.Printf("Received message from agent: %s\n\n\n", string(m.Data))
+	})
+
+	startwg.Wait()
 }
+
+
+
+
